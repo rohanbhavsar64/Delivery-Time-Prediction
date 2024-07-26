ebb342efdc2dd014e1d03045b40e3738791963e6
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-26 11:53:40 +0530

    Update ti.py

--- ti.py
+++ ti.py
@@ -223,44 +223,42 @@ else:
     with col3:
         o=st.slider('% OFF You Need',min_value=0, max_value=100, value=10, step=10)
     x=df[(df['offer_above']<=r)&(df['offer_percentage']>=o)&(df['location']==n)].groupby('hotel_name')['time_minutes'].mean().nlargest(10)
-    fig1=px.line(x)
-    fig1.update_layout(
-        title={
-            'text': "Hotel Vs Time Taken",
-            'y':0.9,
-            'x':0.9,
-            'xanchor': 'right',
-            'yanchor': 'top'})
-    fig1.update_layout(
-        title="Hotel Vs Time Taken",
-        xaxis_title="Hotel Name",
-        yaxis_title="Time Of Delivery",
-        
-        
-        )
     if x.shape[0]!=0:
+        fig1=px.line(x)\
+        fig1.update_layout(
+            title={
+                'text': "Hotel Vs Time Taken",
+                'y':0.9,
+                'x':0.9,
+                'xanchor': 'right',
+                'yanchor': 'top'})
+        fig1.update_layout(
+            title="Hotel Vs Time Taken",
+            xaxis_title="Hotel Name",
+            yaxis_title="Time Of Delivery",
+        )
         st.write(fig1)
     y=df[(df['offer_above']<=r)&(df['offer_percentage']>=o)&(df['location']==n)].groupby('hotel_name')['rating'].mean().nlargest(10)
-    fig2=px.line(y)
-    fig2.update_layout(
-        title={
-            'text': "Hotel Vs Rating",
-            'y':0.9,
-            'x':0.9,
-            'xanchor': 'right',
-            'yanchor': 'top'})
-    fig2.update_layout(
-        title="Hotel Vs Rating",
-        xaxis_title="Hotel Name",
-        yaxis_title="Rating",
-        legend_title="Legend Title",
-        font=dict(
-            family="Courier New, monospace",
-            size=18,
-            color="RebeccaPurple"
-        )
-        )
-    if x.shape[0]!=0:
+    if y.shape[0]!=0:  
+        fig2=px.line(y)
+        fig2.update_layout(
+            title={
+                'text': "Hotel Vs Rating",
+                'y':0.9,
+                'x':0.9,
+                'xanchor': 'right',
+                'yanchor': 'top'})
+        fig2.update_layout(
+            title="Hotel Vs Rating",
+            xaxis_title="Hotel Name",
+            yaxis_title="Rating",
+            legend_title="Legend Title",
+            font=dict(
+                family="Courier New, monospace",
+                size=18,
+                color="RebeccaPurple"
+            )
+            )
         st.write(fig1)
     
     
